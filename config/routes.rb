a0e1af01178a10aba75e0f718dc2de5e1cc4c2e4
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-10-31 14:03:20 +0400

    build Sessions controller, new view and login/out routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'static_pages#home'
+  root    'static_pages#home'
+
+  get     '/new_league' => 'leagues#new'
+  post    '/leagues'    => 'leagues#create'
+
+  get     'login'       => 'sessions#new'
+  post    'login'       => 'sessions#create'
+  delete  'logout'      => 'sessions#destroy'
+
   resources :matches
-  get '/new_league' => 'leagues#new'
-  post '/leagues' => 'leagues#create'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

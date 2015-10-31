81e381db1e805ab1a02b7e5b70dec8cb20a3a36a
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-10-31 16:09:49 +0400

    cannot login with invalid info

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	league = League.find_by(name: params[:session][:name])
-  	log_in(league)
-  	redirect_to root_path
-  	flash[:notice] = "You are logged in to #{current_league.name}"
+  	league = League.find_by(name: params[:session][:name]) 
+  	if league && league.authenticate(params[:session][:password])
+	  	log_in(league)
+	  	redirect_to root_path
+	  	flash[:notice] = "You are logged in to #{current_league.name}"
+	  else
+	  	flash[:alert] = "Invalid League Name/Password combination"
+	  	render 'new'
+		end
   end
 end

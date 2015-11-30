4f67aaf4e09a3372bcbc3021d9b72ee7cf895442
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-11-30 17:24:53 +0400

    log_out method written tested

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,10 @@ module SessionsHelper
 		session[:league_id] = league.id
 	end
 
+	def log_out
+		session.delete(:league_id)
+	end
+
 	def current_league
 		@current_league ||= League.find_by(id: session[:league_id])
 	end
--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 
 describe SessionsHelper do
+	let(:league){ create :league }
+
 	describe '#log_in' do
 		it 'places the league in the session' do
-			league = create(:league)
 			log_in(league)
 			expect(session[:league_id]).to eq league.id
+		end
+	end
+
+	describe '#log_out' do
+		it 'removes a league from the session' do
+			log_in(league)
+			log_out
+			expect(session[:league_id]).to eq nil
 		end
 	end
 end
